962d24e580e7314b88acd263b7995bbfecbba5a7
Said <saidziani.pro@gmail.com>
2018-04-07 17:28:08 +0100

    adding global style.

---
+++ Style.js
@@ -0,0 +1,33 @@
+const white = '#f5f5f5'
+const black = '#0c0c0c'
+const gray = '#424242'
+export default {
+    white: white,
+    black: black,
+    gray: gray,
+    container:{
+        margin: 20,
+    },
+    title:{
+        fontSize: 25,
+        marginBottom:30
+    },
+    input:{
+        height: 40,
+        borderColor: white,
+        borderWidth: 1,
+        paddingHorizontal: 15,
+        marginBottom:20
+    },
+    text:{
+        fontSize:15,
+    },
+    header:{
+        backgroundColor: black,
+    },
+    headerTitle:{
+        fontSize: 25,
+        color: white
+    }
+
+}
